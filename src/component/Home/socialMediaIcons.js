5f12ac9b5f5c7b18bc696e637c90c03ecfe633a6
Omar Eltabakh <omar.mohamed.eltabakh1@gmail.com>
2026-03-22 02:37:05 +0200

    update facebook link

--- src/component/Home/socialMediaIcons.js
+++ src/component/Home/socialMediaIcons.js
@@ -1,6 +1,6 @@
 export const socialMediaIcons = [
     { name: "fa-github", link: "https://github.com/OmarEltabakh", marginRate: "me-2" },
     { name: " fa-linkedin-in", link: "https://www.linkedin.com/in/omr-al-tabakh/", marginRate: "mx-2" },
-    { name: " fa-facebook", link: "", marginRate: "mx-2" },
+    { name: " fa-facebook", link: "https://www.facebook.com/profile.php?id=61579495358235", marginRate: "mx-2" },
 
 ]
